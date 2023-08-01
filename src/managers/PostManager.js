feabf0a939820f4f91d932b6f42ea016c106f281
Brandon Highland <coffeemonsterstudio@gmail.com>
2023-08-01 13:45:41 -0500

    feat: added post details page

--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -2,3 +2,8 @@ export const getPosts = () => {
   return fetch("http://localhost:8088/posts")
     .then(res => res.json())
 }
+
+export const getSinglePost = (postId) => {
+  return fetch(`http://localhost:8088/posts/${postId}`)
+    .then(res => res.json())
+}
