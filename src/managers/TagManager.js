9267c3a0403d2ba82df95eb06ac3bed38c736b61
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-08-01 21:39:14 -0400

    Tag List With Edit and Delete Buttons

--- src/managers/TagManager.js
+++ src/managers/TagManager.js
@@ -1,4 +1,10 @@
 export const getAllTags = () => {
   return fetch("http://localhost:8088/tags")
-    .then(res => res.json())
-}
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to fetch tags.");
+      }
+    });
+};
