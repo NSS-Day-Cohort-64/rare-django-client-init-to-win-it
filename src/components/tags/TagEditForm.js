c23e7cf4e80e46ccaeb151fb89d7645529039934
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-29 00:18:02 -0500

    added tag list create/edit/delete

--- src/components/tags/TagEditForm.js
+++ src/components/tags/TagEditForm.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export const TagForm = ({ handleCreateTag }) => {
+export const TagEditForm = ({ tagId, initialLabel, handleUpdateTag }) => {
   const [tagLabel, setTagLabel] = useState('');
+
+  // Populate the initial value when the component mounts
+  useEffect(() => {
+    setTagLabel(initialLabel);
+  }, [initialLabel]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newTag = {
+
+    const updatedTag = {
       label: tagLabel
     };
 
-    handleCreateTag(newTag)
-      .then(() => {
-        setTagLabel(''); // Clear the input field
-      })
-      .catch((error) => {
-        console.error("Failed to create tag:", error);
-      });
+    handleUpdateTag(tagId, updatedTag);
+    setTagLabel(''); // Optionally clear the input field
   };
 
   return (
     <div className="form">
-      <h2 className="form-title">Create Tag</h2>
+      <h2 className="form-title">Edit Tag</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="tagLabel">Name:</label>
         <input
@@ -31,7 +32,7 @@ export const TagForm = ({ handleCreateTag }) => {
           required
         />
         <br />
-        <button type="submit">Create Tag</button>
+        <button type="submit">Update Tag</button>
       </form>
     </div>
   );
--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -1,60 +1,97 @@
-import { useState, useEffect } from "react"
-import { Link, useNavigate } from 'react-router-dom'
-import { getAllTags, createTag } from "../../managers/TagManager"
-import { TagForm } from "./TagForm";
-import "./tags.css"
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getAllTags, createTag, deleteTag, updateTag } from '../../managers/TagManager';
+import { TagForm } from './TagForm';
+import { TagEditForm } from './TagEditForm';  // Import TagEditForm
+import './tags.css';
 
 export const TagList = () => {
+  const [tags, setTags] = useState([]);
+  const [editingTagId, setEditingTagId] = useState(null);
+  const [editingTagLabel, setEditingTagLabel] = useState('');
 
-  const [tags, setTags] = useState([])
-
-  useEffect(
-    () => {
-      getAllTags().then((tagData) => setTags(tagData))
-    },
-    []
-  )
+  useEffect(() => {
+    getAllTags().then((tagData) => setTags(tagData));
+  }, []);
 
   const handleCreateTag = (newTag) => {
-    createTag(newTag)
+    return createTag(newTag)
       .then((response) => {
-        console.log('API Response:', response);
         if (response && response.id) {
-          // Add the new tag to the existing categories array
           const updatedTags = [...tags, response];
-          // Sort the tags alphabetically by label before updating the state
           updatedTags.sort((a, b) => a.label.localeCompare(b.label));
           setTags(updatedTags);
         } else {
-          throw new Error('Failed to create tag. Please try again later.');
+          throw new Error('Failed to create tag.');
         }
+      });
+  };
+
+  const handleDeleteTag = (tagId) => {
+    deleteTag(tagId)
+      .then(() => {
+        const updatedTags = tags.filter(tag => tag.id !== tagId);
+        setTags(updatedTags);
       })
       .catch((error) => console.error(error));
   };
 
-  return (
+  const handleUpdateTag = (tagId, updatedTag) => {
+    updateTag(tagId, updatedTag)
+      .then(() => {
+        const updatedTags = tags.map(tag => {
+          if (tag.id === tagId) {
+            return { ...updatedTag, id: tagId };  // Make sure the id remains the same
+          }
+          return tag;
+        });
+        setTags(updatedTags);
+      })
+      .catch((error) => console.error(error));
+  };
 
+  const startEditing = (tagId, label) => {
+    setEditingTagId(tagId);
+    setEditingTagLabel(label);
+  };
+
+  const stopEditing = () => {
+    setEditingTagId(null);
+    setEditingTagLabel('');
+  };
+
+  return (
     <div className="page-container">
       <h1 className="page-header">Tags</h1>
       <div className="tag-container">
         <div className="left-side">
           <ul className="list">
             {tags.map((tag) => (
-              <li key={tag.id} className="list-items">
+              <li key={tag.id} className="list-items">  {/* Ensure each tag has a unique key */}
                 <div className="list-name">{tag.label}</div>
                 <div className="edit-and-delete">
-                  <button className="edit-button"><Link to={`/tags/${tag.id}/edit`}>Edit</Link></button>{" "}
-                  <button className="delete-button">Delete</button>
+                  <button className="edit-button" onClick={() => startEditing(tag.id, tag.label)}>Edit</button>{" "}
+                  <button className="delete-button" onClick={() => handleDeleteTag(tag.id)}>Delete</button>
                 </div>
               </li>
             ))}
           </ul>
         </div>
         <div className="right-side">
-          <TagForm handleCreateTag={handleCreateTag} />
+          {editingTagId ? (
+            <TagEditForm
+              tagId={editingTagId}
+              initialLabel={editingTagLabel}
+              handleUpdateTag={(tagId, updatedTag) => {
+                handleUpdateTag(tagId, updatedTag);
+                stopEditing();
+              }}
+            />
+          ) : (
+            <TagForm handleCreateTag={handleCreateTag} />
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
