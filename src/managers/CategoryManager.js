1bd03f7c63676f819f3359af62de68148074af5f
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-29 02:11:26 -0500

    8/30 1029, added add/edit/delete categories and alphabetized list

--- src/managers/CategoryManager.js
+++ src/managers/CategoryManager.js
@@ -1,32 +1,65 @@
-const getAuthHeaders = () => ({
-    "Authorization": `Token ${localStorage.getItem("auth_token")}`,
-    "Content-Type": "application/json"
-  });
-  
-  export const createCategory = (newCategory) => {
+export const createCategory = (newCategory) => {
     return fetch("http://localhost:8000/categories", {
       method: "POST",
-      headers: getAuthHeaders(),
+      headers: {
+        "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(newCategory),
     })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Failed to create category.");
-      }
-    });
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to create category.");
+        }
+      });
   };
   
   export const getCategories = () => {
     return fetch("http://localhost:8000/categories", {
-      headers: getAuthHeaders()
+      method: "GET",
+      headers: {
+        "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to fetch categories.");
+        }
+      });
+  };
+  
+  
+  export const deleteCategory = (categoryId) => {
+    return fetch(`http://localhost:8000/categories/${categoryId}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+      },
     })
     .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Failed to fetch categories.");
+      if (!response.ok) {
+        throw new Error("Failed to delete category.");
+      }
+    });
+  };
+  
+  export const updateCategory = (categoryId, updatedCategory) => {
+    return fetch(`http://localhost:8000/categories/${categoryId}`, {
+      method: "PUT",
+      headers: {
+        "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedCategory),
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to update category.");
       }
     });
   };
