eb8168d634ef660c8c07e452fb25ae0828faaf7c
Logan Welch <loganewelch@gmail.com>
2023-08-30 14:15:53 -0500

    added JSX to show comments on post details page

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -1,26 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getSinglePost } from '../../managers/PostManager'
+import { getPostComments } from '../../managers/CommentManager'
 
 function PostDetails() {
   const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
   const { postId } = useParams()
 
-  useEffect(() => {
-    getSinglePost(postId)
-      .then(setPost)
-  }, [postId])
+useEffect(() => {
+  getSinglePost(postId)
+    .then(setPost)
+  getPostComments(postId)
+    .then(setComments)
+}, [postId])
+
+
 
   return (
     <section className="post">
       <h3 className="post__title">{post.title}</h3>
-      <div className="post__author">{post.author?.first_name} {post.author?.last_name}</div>
-      <div className="post__category">{post.category?.label}</div>
-      <div className="post__publicationDate">{post.publication_date}</div>
+      <div className="post__author">Author: {post.author?.full_name}</div>
+      <div className="post__category">Category: {post.category?.label}</div>
+      <div className="post__publicationDate">Publication Date: {post.publication_date}</div>
       <br />
       <div className="post__content">{post.content}</div>
+      <br />
+      <h4>Comments:</h4>
+      <ul className="comments_list">
+        {comments.map(comment => (
+          <li key={comment.id} className="comment">
+            <div className="comment__author">{comment.author?.full_name}</div>
+            <div className="comment__timestamp">{comment.time_stamp}</div>
+            <div className="comment__content">{comment.content}</div>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
 
 export default PostDetails
+
+
+// useEffect(() => {
+//   getSinglePost(postId)
+//     .then(setPost);
+//   getPostComments(postId)
+//     .then(setComments);
+// }, [postId])
--- src/managers/CommentManager.js
+++ src/managers/CommentManager.js
@@ -1,9 +1,19 @@
+const getAuthHeaders = () => ({
+    "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+    "Content-Type": "application/json"
+});
+
 export const createComment = (newComment) => {
     return fetch("http://localhost:8000/comments", {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
+        headers: getAuthHeaders(),
         body: JSON.stringify(newComment),
     }).then((response) => response.json())
 }
+
+export const getPostComments = (postId) => {
+    return fetch(`http://localhost:8000/comments?post_id=${postId}`, {
+        headers: getAuthHeaders()
+    })
+        .then(res => res.json());
+};
