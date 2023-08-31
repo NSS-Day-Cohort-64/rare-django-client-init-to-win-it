6e985de6494589f94dad0eef23b069caaf7087be
Casey Ferebee <cwferebee@gamil.com>
2023-08-31 10:13:40 -0500

    working on editpost, tags are the only thing left, added editPostForm to application view

--- src/managers/CategoryManager.js
+++ src/managers/CategoryManager.js
@@ -1,66 +1,66 @@
 export const createCategory = (newCategory) => {
     return fetch("http://localhost:8000/categories", {
-      method: "POST",
-      headers: {
-        "Authorization": `Token ${localStorage.getItem("auth_token")}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newCategory),
+        method: "POST",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newCategory),
     })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Failed to create category.");
-        }
-      });
-  };
-  
-  export const getCategories = () => {
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Failed to create category.");
+            }
+        });
+};
+
+export const getCategories = () => {
     return fetch("http://localhost:8000/categories", {
-      method: "GET",
-      headers: {
-        "Authorization": `Token ${localStorage.getItem("auth_token")}`,
-        "Content-Type": "application/json",
-      },
+        method: "GET",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+            "Content-Type": "application/json",
+        },
     })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Failed to fetch categories.");
-        }
-      });
-  };
-  
-  
-  export const deleteCategory = (categoryId) => {
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Failed to fetch categories.");
+            }
+        });
+};
+
+
+export const deleteCategory = (categoryId) => {
     return fetch(`http://localhost:8000/categories/${categoryId}`, {
-      method: "DELETE",
-      headers: {
-        "Authorization": `Token ${localStorage.getItem("auth_token")}`,
-      },
+        method: "DELETE",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+        },
     })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to delete category.");
-      }
-    });
-  };
-  
-  export const updateCategory = (categoryId, updatedCategory) => {
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to delete category.");
+            }
+        });
+};
+
+export const updateCategory = (categoryId, updatedCategory) => {
     return fetch(`http://localhost:8000/categories/${categoryId}`, {
-      method: "PUT",
-      headers: {
-        "Authorization": `Token ${localStorage.getItem("auth_token")}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedCategory),
+        method: "PUT",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedCategory),
     })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to update category.");
-      }
-    });
-  };
-  
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to update category.");
+            }
+        });
+};
+
